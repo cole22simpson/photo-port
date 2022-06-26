55dfd302ab5ccd9b96a85cc386e0bd8f6b25caa2
Cole Simpson <cole22simpson@gmail.com>
2022-06-25 21:36:29 -0700

    galery displays photos

--- components/PhotoList/index.js
+++ components/PhotoList/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
-const PhotoList =({ category }) => {
+const PhotoList = ({ category }) => {
+
   const [photos] = useState([
     {
       name: 'Grocery aisle',
@@ -98,15 +99,15 @@ const PhotoList =({ category }) => {
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
     },
   ]);
-  
+
   const currentPhotos = photos.filter((photo) => photo.category === category);
-  
+
   return (
     <div>
       <div className="flex-row">
-        {photos.map((image, i) => (
+        {currentPhotos.map((image, i) => (
           <img
-            src={require(`../../assets/small/${category}/${i}.jpg`).default}
+            src={require(`../../assets/small/${category}/${i}.jpg`)}
             alt={image.name}
             className="img-thumbnail mx-1"
             key={image.name}
@@ -115,6 +116,6 @@ const PhotoList =({ category }) => {
       </div>
     </div>
   );
-}
+};
 
 export default PhotoList;
