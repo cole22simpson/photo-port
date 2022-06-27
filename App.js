4d905ac7690548f6b62f1867cb1e944c41f8d46b
Cole Simpson <cole22simpson@gmail.com>
2022-06-26 19:52:31 -0700

    ran build

--- App.js
+++ App.js
@@ -17,6 +17,7 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
@@ -24,13 +25,18 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <ContactForm></ContactForm>
-        <div>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-        </div>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
